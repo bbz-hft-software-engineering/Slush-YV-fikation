e6626cdf96a1678d26c241caefbf2a8ea454946b
Mobileplayer <suter.eric@hotmail.com>
2023-04-19 15:49:10 +0200

    Bugfixes
    Version which is half bootable

--- main.c
+++ main.c
@@ -25,6 +25,7 @@
 // Settings
 #define SENSORSET 10
 #define SPEED 40
+#define CALFLOW 50
 
 // Drinks
 #define WATER 0
@@ -32,8 +33,8 @@
 #define HOLUNDER 2
 
 // Sizes
-#define SMALL 0
-#define MEDIUM 1
+#define SMALL 1
+#define MEDIUM 2
 #define LARGE 3
 
 // Menu points
@@ -41,6 +42,20 @@
 #define SIZESELECT 1
 #define WORKINPROGRESS 2
 
+// Drink 1 Receipe in %
+#define D1L1 15
+#define D1L2 85
+#define D1L3 0
+
+// Drink 2 Receipe in %
+#define D2L1 0
+#define D2L2 100
+#define D2L3 0
+
+// Drink 3 Receipe in %
+#define D3L1 0
+#define D3L2 85
+#define D3L3 15
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -50,12 +65,16 @@
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 int status = WAIT;
-int sensStart = 500;
-int sensFill = 500;
+float sensStart = 500;
+float sensFill = 500;
 int drink = 0;
 int size = 0;
 int menu = DRINKSELECT;
 int cancel = 0;
+
+int l1ready = 0;
+int l2ready = 0;
+int l3ready = 0;
 
 // Flankenerkennung
 int backLeft = 0;
@@ -96,25 +115,29 @@ void FlankHandler(void);
 task main(){
 
 	drawBmpfile(0, 127, "startScreen"); // Show splash screen
+	resetMotorEncoder(L1Mot);
+	resetMotorEncoder(L2Mot);
+	resetMotorEncoder(L3Mot);
+
 	delay(2000);	// Wait 2 seconds
 
 	while(1){	// repeating loop
 
 		FlankHandler();
 		HandleMenu();
+		sensStart = getUSDistance(StartSens);
+		sensFill = getUSDistance(FillSens);
 
 		if (status != WAIT){	// Task active?
 
-			sensStart = getUSDistance(StartSens);
-			sensFill = getUSDistance(FillSens);
+			if((status == EMPTY) && (SENSORSET < sensFill)){
 
-			if((status == EMPTY) && (SENSORSET >= sensFill)){
-
-				status = MoveBelt(LEFT,SPEED);
+				status = MoveBelt(RIGHT,SPEED);
 
 			}
-			if((status == EMPTY) && (SENSORSET <= sensFill)){
+			if((status == EMPTY) && (SENSORSET > sensFill)){
 
+				setMotorSpeed(BeltMot,0);
 				status = FILLING;
 
 			}
@@ -125,7 +148,7 @@ task main(){
 			}
 			if((status == FULL) && (SENSORSET <= sensStart)){
 
-				status = MoveBelt(RIGHT,SPEED);
+				status = MoveBelt(LEFT,SPEED);
 
 			}
 		}
@@ -142,34 +165,25 @@ task main(){
 
 int MoveBelt(int setDirection, int setSpeed){
 
-	if( setDirection == 1){
+	if( setDirection == RIGHT){
 
-		if(sensStart >= SENSORSET){
+		if(SENSORSET >= sensStart  ){
 
 			setMotorSpeed(BeltMot,SPEED);
-
-		}
-		else{
-
-			setMotorSpeed(BeltMot,0);
+			return EMPTY;
 
 		}
 	}
+	return EMPTY;
+	if(setDirection == LEFT){
 
-	else{
-
-		if(sensFill >= SENSORSET){
+		if(SENSORSET <= sensFill ){
 
 			setMotorSpeed(BeltMot,(SPEED * -1));
-
-		}
-		else{
-
-			setMotorSpeed(BeltMot,0);
+			return FULL;
 
 		}
 	}
-	return 0;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -181,9 +195,137 @@ int MoveBelt(int setDirection, int setSpeed){
 
 int DispenseDrink(int chosenDrink, int drinkSize){
 
+	int l1motget = getMotorEncoder(L1Mot);
+ 	int l2motget = getMotorEncoder(L2Mot);
+	int l3motget = getMotorEncoder(L3Mot);
+
+	switch(DRINKSELECT){
+
+		case WATER:
+			if((size * D1L1 * CALFLOW) > l1motget){
+
+				setMotorSpeed(L1Mot, 100);
+
+			}
+			else{
+
+				setMotorSpeed(L1Mot, 0);
+				l1ready = 1;
+
+			}
+			if((size * D1L2 * CALFLOW) > l2motget){
+
+				setMotorSpeed(L2Mot, 100);
+
+			}
+			else{
+
+				setMotorSpeed(L2Mot, 0);
+				l2ready = 1;
+
+			}
+			if((size * D1L3 * CALFLOW) > l3motget){
+
+				setMotorSpeed(L3Mot, 100);
+
+			}
+			else{
+
+				setMotorSpeed(L3Mot, 0);
+				l3ready = 1;
+
+			}
+		break;
 
 
+		case STRAWBERRY:
+			if((size * D2L1) >= l1motget){
 
+				setMotorSpeed(L1Mot, 100);
+
+			}
+			else{
+
+				setMotorSpeed(L1Mot, 0);
+				l1ready = 1;
+
+			}
+			if((size * D2L2) >= l2motget){
+
+				setMotorSpeed(L2Mot, 100);
+
+			}
+			else{
+
+				setMotorSpeed(L2Mot, 0);
+				l2ready = 1;
+
+			}
+			if((size * D2L3) >= l3motget){
+
+				setMotorSpeed(L3Mot, 100);
+
+			}
+			else{
+
+				setMotorSpeed(L3Mot, 0);
+				l3ready = 1;
+
+			}
+		break;
+		case HOLUNDER:
+			if((size * D3L1) >= l1motget){
+
+				setMotorSpeed(L1Mot, 100);
+
+			}
+			else{
+
+				setMotorSpeed(L1Mot, 0);
+				l1ready = 1;
+
+			}
+			if((size * D3L2) >= l2motget){
+
+				setMotorSpeed(L2Mot, 100);
+
+			}
+			else{
+
+				setMotorSpeed(L2Mot, 0);
+				l2ready = 1;
+
+			}
+			if((size * D3L3) >= l3motget){
+
+				setMotorSpeed(L3Mot, 100);
+
+			}
+			else{
+
+				setMotorSpeed(L3Mot, 0);
+				l3ready = 1;
+
+			}
+		break;
+
+	}
+
+	if(l1ready && l2ready && l3ready){
+
+		resetMotorEncoder(L1Mot);
+		resetMotorEncoder(L2Mot);
+		resetMotorEncoder(L3Mot);
+
+		l1ready = 0;
+		l2ready = 0;
+		l3ready = 0;
+
+		return FULL;
+	}
+	else{
+		return FILLING;
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -252,8 +394,9 @@ void HandleMenu(void){
 		case WORKINPROGRESS:
 
 		 drawBmpfile(0, 127, "workInProgress");
-
-
+			if (status == WAIT){
+		 		status = EMPTY;
+			}
 		 if(triggerMiddle){
 
 				menu = DRINKSELECT;
@@ -263,10 +406,11 @@ void HandleMenu(void){
 		break;
 	}
 
-	if ( status == FULL && sensStart <= SENSORSET){
+	if ((status == FULL) && sensStart < SENSORSET){
 
 		menu = DRINKSELECT;
 		status = WAIT;
+
 
 	}
 
@@ -303,6 +447,7 @@ void FlankHandler(void){
 	else{ triggerMiddle = 0; }
 
 	if (nowRight == 1 && backRight == 0){
+
 
 		triggerRight = 1;
 
